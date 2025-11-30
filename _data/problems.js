c678695b5284efa48b4b89489deb4a539ed50c02
Djoystick <j.j.niccals2@gmail.com>
2025-11-30 09:45:14 +0300

    Delete _data directory

--- _data/problems.js
+++
@@ -1,9 +0,0 @@
-// Генерируется автоматически
-window.problems = [];
-fetch('../_data/problems.json')
-  .then(r => r.json())
-  .then(data => {
-    window.problems = data;
-    if (typeof render === 'function') render(data);
-  })
-  .catch(console.error);
